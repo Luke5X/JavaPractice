629c864005192b168e377b414663e2ad1008ffe5
Luke Bednash <ldbmlg@gmail.com>
2020-05-01 02:15:08 -0700

    this is my first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//my first program
+        //System.out.println("i like minecraft");
+        int health=4;
+        System.out.println(health);
+        health=health-1;
+        System.out.println(health);
+        health=health*3;
+        System.out.println(health);
+
+    }
+}
