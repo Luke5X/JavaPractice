01498acd3c6c80bd8ae1e4bd4c8f5b348ad0d294
Luke Bednash <ldbmlg@gmail.com>
2020-05-02 00:41:06 -0700

    Learned about loops and arrays

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	//my first program
-        //System.out.println("i like minecraft");
-        int health=4;
-        System.out.println(health);
-        health=health-1;
-        System.out.println(health);
-        health=health*3;
-        System.out.println(health);
+//	my first program
+//        System.out.println("i like minecraft");
+//        int health=4;
+//        System.out.println(health);
+//        health=health-1;
+//        System.out.println(health);
+//        health=health*3;
+//        System.out.println(health);
+//
+//        int mana;
+//        mana=100;
+//        System.out.println(mana);
+//        mana=mana*5;
+//        System.out.println(mana);
 
+            String welcominggreeting ="yo";
+            int numberOfTimes=200;
+            for (int j=0; j < numberOfTimes; j=j+1) {
+
+                System.out.println(j);
+            }
+            welcominggreeting=welcominggreeting+"im learning coding";
+            System.out.println(welcominggreeting);
     }
 }
